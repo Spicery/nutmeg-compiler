2d353af91700dea78b56abbf23014e1d97d07544
Stephen Leach <sfkleach@gmail.com>
2025-09-29 07:59:13 +0100

    Remove relic code

--- pkg/rewriter/config.go
+++ pkg/rewriter/config.go
@@ -6,18 +6,6 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type ExtendedConfigurableOptions struct {
-	Format string `yaml:"option-format,omitempty"`
-	Indent int    `yaml:"option-indent,omitempty"`
-	// DefaultLabel      string `yaml:"option-default-label,omitempty"`
-	IncludeSpans  bool `yaml:"option-include-spans,omitempty"`
-	Decimal       bool `yaml:"option-decimal,omitempty"`
-	CheckLiterals bool `yaml:"option-check-literals,omitempty"`
-	// UseClassifier     string `yaml:"option-use-classifier,omitempty"`
-	TrimTokenOnOutput int            `yaml:"option-trim-token-on-output,omitempty"`
-	Substitutions     *Substitutions `yaml:"-"` // Don't serialize, loaded separately
-}
-
 type RewriteConfig struct {
 	Substitutions *Substitutions `yaml:"substitutions,omitempty"`
 }
